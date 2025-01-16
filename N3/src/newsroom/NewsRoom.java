e5bd6b4b11e7f06e042c5395978f878d45bb3732
Aurélien Darbellay <aurlien.darbellay@gmail.com>
2025-01-16 08:45:12 +0100

    16.01.2024

--- N3/src/newsroom/NewsRoom.java
+++ N3/src/newsroom/NewsRoom.java
@@ -17,7 +17,6 @@ abstract public class NewsRoom {
         System.out.println("Great! We've hired " + name + "!");
         redactors.add(newRedactor);
     }
-
     public static void eliminateRedactor() {
         System.out.println("What is the DNI of the redactor you want to fire?");
         String dni = scanner.nextLine();
@@ -25,19 +24,53 @@ abstract public class NewsRoom {
         System.out.println(result ? "Redactor fired" : "redactor not found");
 
     }
-
     public static void showStaff() {
         for (Redactor redactor : redactors) {
             System.out.println(redactor.getName() + " " + redactor.getDni());
         }
     }
-
-    public static void createNews() {
+    private static News createNews(){
+        System.out.println("What is the title of the news you want to create");
+        String title= scanner.nextLine();
+        System.out.println("""
+                What is the subject of the news you want to create?
+                Soccer [S]
+                Tennis [T]
+                Basket [B]
+                F1 [F]
+                Moto [M]
+                """);
+        String topic = scanner.nextLine();
+        return switch (topic) {
+            case "S" -> News.createSoccerNews(title);
+            case "T" -> News.createTennisNews(title);
+            case "B" -> News.createBasketNews(title);
+            case "F" -> News.createF1News(title);
+            case "M" -> News.createMotoNews(title);
+            default -> {
+                System.out.println("Topic not found. We are creating a news about soccer by default");
+                yield News.createSoccerNews(title);
+            }
+        };
+    }
+    private static Redactor getRedactorByDni(String dni){
+        Optional<Redactor> redactorOptional = redactors.stream().filter(item -> item.getDni().equals(dni)).findFirst();
+        if (redactorOptional.isEmpty()) {
+            throw new Error ("Redactor not found");
+        }
+        return redactorOptional.get();
+    }
+    public static void assignNews() {
         System.out.println("What is the DNI of the redactor you want to work on this new piece of news?");
         String dni = scanner.nextLine();
-        Optional<Redactor> redactor = redactors.stream().filter(item -> item.getDni().equals(dni)).findFirst();
-
-
+        try {
+            Redactor redactor = getRedactorByDni(dni);
+            News pNews = createNews();
+            redactor.addNews(pNews);
+        }catch(Error e){
+            System.out.println(e.getMessage());
+            return;
+        }
     }
 
     public static void eliminateNews() {
@@ -45,19 +78,29 @@ abstract public class NewsRoom {
     }
 
     public static void showNews() {
+        System.out.println("What is the redactor whose workload you want to inspect? Give me their DNI.");
+        String dni = scanner.nextLine();
+        try{
+            Redactor redactor = getRedactorByDni(dni);
+            ArrayList<News> news = redactor.getNews();
+            System.out.println(redactor.getName() + " is currently working on the following news:");
+            for (News pNews : news){
+                System.out.println(pNews.getTitle());
+            }
+        }catch (Error e){
+            System.out.println(e.getMessage());
+            return;
+        }
 
     }
 
     public static void updateNews() {
-
     }
 
     public static void getNewsPrice() {
-
     }
 
     public static void getNewsValue() {
-
     }
 
     public static ArrayList<Redactor> getRedactors() {
